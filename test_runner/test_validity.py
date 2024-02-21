691b00d11935efdcee3c8360e669dbffbe2e9d60
Henrik Ginnerup <hginnerup@hotmail.com>
2024-02-21 18:06:26 +0100

    Added hierarchical color translation

--- test_runner/test_validity.py
+++ test_runner/test_validity.py
@@ -6,7 +6,7 @@ def get_validity_code(domain_path: str, problem_path: str) -> int:
     domain_path_abs = os.path.abspath(domain_path)
     problem_path_abs = os.path.abspath(problem_path)
 
-    validity_process = subprocess.run(["python3", "test_runner/systems/colored_translation/main.py", "--testValidity", domain_path_abs, problem_path_abs, "_", "_"])
+    validity_process = subprocess.run(["python3", "test_runner/systems/colored_translation_hierarchy/main.py", "--testValidity", domain_path_abs, problem_path_abs, "_", "_"])
 
     validity_code = validity_process.returncode
 
--- test_runner/translators/__init__.py
+++ test_runner/translators/__init__.py
@@ -5,6 +5,7 @@ from .tapaal_translator import TapaalTranslator
 from .tapaal_searcher import TapaalSearcher
 from .tapaal_color_searcher import TapaalColorSearcher
 from .lifted_translator import LiftedTranslator
+from .lifted_hierarchy_translator import LiftedHierarchyTranslator
 from .grounded_translator import GroundedTranslator
 
 from .downward_translator import DownwardTranslator
