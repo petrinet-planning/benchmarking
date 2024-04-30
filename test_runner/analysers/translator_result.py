722705f15d6090c0586eb006e61a0d7e238d3680
sebastianlassen00 <slasse19@student.aau.dk>
2024-04-30 11:33:37 +0200

    Translation exceptions parsed

--- test_runner/analysers/translator_result.py
+++ test_runner/analysers/translator_result.py
@@ -1,6 +1,11 @@
 from .base_result import BaseResult
-
+import re
 
 class TranslatorResult(BaseResult):
     search_results: dict["BaseSearcher", list["SearchResult"]]
+    valid_translation: bool
+
+    def parse_result(self, file_content: str, test_case: "TestCase", print_unfound_keys: bool = False) -> None:
+        super().parse_result(file_content, test_case, print_unfound_keys)
+        self.valid_translation = not bool(re.search(r"exception", file_content, re.IGNORECASE))
     
