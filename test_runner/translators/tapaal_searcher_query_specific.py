492b99b5733975b134f683c867acdd8b4f7b4c63
Henrik Ginnerup <hginnerup@hotmail.com>
2024-05-31 11:05:08 +0200

    Hotfix for xml-queries being off by 1

--- test_runner/translators/tapaal_searcher_query_specific.py
+++ test_runner/translators/tapaal_searcher_query_specific.py
@@ -7,4 +7,4 @@ class TapaalSearcher_QuerySpecific(TapaalSearcher):
     def do_search(self, translator: "TapaalTranslator", test_case: "TestCase_c2p") -> str:
         baseStr = super().do_search(translator, test_case)
         
-        return f"{baseStr} --xml-queries {test_case.query_id}"
+        return f"{baseStr} --xml-queries {test_case.query_id - 1}"
